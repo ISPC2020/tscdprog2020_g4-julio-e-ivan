8c7916b1d9410594d481251e624d9665e95e66dd
Ivano05 <ivan.augusto08@gmail.com>
2021-08-07 23:15:10 -0300

    Select Insert

--- cliente.py
+++ cliente.py
@@ -1,17 +1,40 @@
+from conexion import Conexion
+
 class Cliente:
-    def __init__(self, contacto):
-        self.contactos = contacto
+    def __init__(self):
+        objetoConexion = Conexion().conectar()
+        colectorDeDatos = objetoConexion.cursor() #metodo cursor esta retornado desde la libreria pymysql
+
+        colectorDeDatos.execute("SELECT * FROM employees limit 10") #por cursor puedo usar execute y hago la consulta a la BD
+
+        resultado = colectorDeDatos.fetchall() #fetchall es un metodo de pymysql que es para traer todos los datos del cursor
+
+        clientes = []
+        for row in resultado:
+            cliente= {"numero_de_cliente":row[0], "nombre_cliente":row[2], "apellido_cliente":row[3], "cuenta_corriente":row[6]}   
+            clientes.append(cliente)
+
+        self.clientes = clientes
 
     def crear(self):
-        # pedir el nombre
-        nombre = input("Ingrese el usuario: ")
+                # pedir el nombre / cambiar por ID de empleado
+        identificacion = int(input("Ingrese su numero de cliente: "))
         # recorrer el listado de los clientes actuales y buscar de que no exista en la base.
-        if not any(contacto['nombre'] == nombre for contacto in self.contactos):
-            nuevoContacto = {'nombre': nombre, 'cantidad': 0}
-            self.contactos.append(nuevoContacto)
+        if not any(cliente['numero_de_cliente'] == identificacion for cliente in self.clientes):
+            nombre = input("Ingrese su nombre: ")
+            apellido = input("Ingrese su apellido: ")
+            #nuevo_cliente = {'numero_de_cliente': identificacion, "nombre_cliente": nombre, "apellido_cliente":apellido, "cuenta_corriente":0}
+           # self.contactos.append(nuevo_cliente)
+            conexion = Conexion().conectar()
+            cursor = conexion.cursor()        
+            sql= "insert into employees (emp_no, first_name, last_name) values ("+str(identificacion)+",'"+nombre+"','"+apellido+"');"
+            cursor.execute(sql)
+            conexion.commit()
+            #INSERT INTO employees (emp_no, first_name, last_name) VALUES (1, 'Demian', 'Torres');
+
             print('Se agrego el usuario: ', nombre)
         else:
-            print('Ya existe un usuario con ese nombre')
+            print('Ya existe un usuario con numero de cliente')
 
     # TODO: Analizar el caso en el que no exista un usuario en la base.
     def depositar(self):
