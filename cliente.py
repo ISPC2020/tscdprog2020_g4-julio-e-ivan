0317997b3170450b1de4abc2f8dd15780f362429
Ivano05 <ivan.augusto08@gmail.com>
2021-08-16 22:28:25 -0300

    funcion depositar

--- cliente.py
+++ cliente.py
@@ -17,7 +17,7 @@ class Cliente:
         self.clientes = clientes
 
     def crear(self):
-                # pedir el nombre / cambiar por ID de empleado
+                # pedir el ID de empleado para identificarlo
         identificacion = int(input("Ingrese su numero de cliente: "))
         # recorrer el listado de los clientes actuales y buscar de que no exista en la base.
         if not any(cliente['numero_de_cliente'] == identificacion for cliente in self.clientes):
@@ -27,27 +27,46 @@ class Cliente:
            # self.contactos.append(nuevo_cliente)
             conexion = Conexion().conectar()
             cursor = conexion.cursor()        
-            sql= "insert into employees (emp_no, first_name, last_name) values ("+str(identificacion)+",'"+nombre+"','"+apellido+"');"
-            cursor.execute(sql)
+            cursor.execute("insert into employees (emp_no, first_name, last_name) values (%s, %s, %s);", (str(identificacion), nombre, apellido))
             conexion.commit() #probar si hay un auto commit
-            #INSERT INTO employees (emp_no, first_name, last_name) VALUES (1, 'Demian', 'Torres');
 
             print('Se agrego el usuario: ', nombre)
         else:
             print('Ya existe un usuario con numero de cliente')
 
-    # TODO: Analizar el caso en el que no exista un usuario en la base.
-    def depositar(self):
-        nombre = input("Ingrese el usuario: ")
-        for contacto in range(len(self.contactos)):
-            if nombre == self.contactos[contacto]['nombre']:
-                platitaUsuario = self.contactos[contacto]['cantidad']
-                valor = int(input("Ingrese cuanto desea depositar: "))
-                platitaUsuario += valor
-                self.contactos[contacto]['cantidad'] = platitaUsuario
-                print(platitaUsuario)
 
-    # TODO: Tiene que ser float
+    def depositar(self):
+        #nombre = input("Ingrese el usuario: ")
+        identificacion = int(input("Ingrese su numero de cliente: ")) #Lo agrego para buscar al cliente
+        conexion = Conexion().conectar()
+        cursor = conexion.cursor()
+        #sql= "SELECT * FROM `employees` WHERE emp_no = "
+        cursor.execute("SELECT * FROM `employees` WHERE emp_no = %s",  (str(identificacion)))
+        resultado = cursor.fetchall()
+        cuenta_corriente_act = float(resultado[0][6])
+        
+        if (len(resultado) > 0):
+            ingreso = float(input("Cuanto desea depositar: "))
+            cuenta_corriente_nva = ingreso + cuenta_corriente_act
+            print("Su cuenta dispone de ", cuenta_corriente_nva)
+            cursor_update = conexion.cursor()
+            cursor_update.execute("UPDATE employees SET cuenta_corriente = %s WHERE emp_no = %s;", (str(cuenta_corriente_nva), str(identificacion)))
+            #UPDATE employees SET cuenta_corriente = 1 WHERE emp_no = 12;
+            conexion.commit() 
+            
+        
+
+        #SELECT * FROM `employees` WHERE emp_no = 06
+
+        #for contacto in range(len(self.contactos)):
+            #if nombre == self.contactos[contacto]['nombre']:
+                #platitaUsuario = self.contactos[contacto]['cantidad']
+                #valor = int(input("Ingrese cuanto desea depositar: "))
+                #platitaUsuario += valor
+                #self.contactos[contacto]['cantidad'] = platitaUsuario
+               # print(platitaUsuario)
+
+
     def extraer(self):
         nombre = input("Ingrese el nombre del usuario: ")
         for contacto in range(len(self.contactos)):
